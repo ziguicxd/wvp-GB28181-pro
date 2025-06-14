a782b3d867b9d11853cd2ccf6086d97edae4658b
ziguicxd <48241692+ziguicxd@users.noreply.github.com>
2025-06-14 21:47:15 +0800

    取消20倍速播放

--- src/main/java/com/genersoft/iot/vmp/service/ICloudRecordService.java
+++ src/main/java/com/genersoft/iot/vmp/service/ICloudRecordService.java
@@ -17,16 +17,26 @@ import java.util.Set;
  */
 public interface ICloudRecordService {
 
+<<<<<<< HEAD
     /**
      * 分页回去云端录像列表
      */
     PageInfo<CloudRecordItem> getList(int page, int count, String query,  String app, String stream, String startTime, String endTime, List<MediaServer> mediaServerItems, String callId, Boolean ascOrder);
+=======
+        /**
+         * 分页回去云端录像列表
+         */
+        PageInfo<CloudRecordItem> getList(int page, int count, String query, String app, String stream,
+                        String startTime,
+                        String endTime, List<MediaServer> mediaServerItems, String callId, Boolean ascOrder);
+>>>>>>> e1caa29e2 (取消20倍速播放)
 
-    /**
-     * 获取所有的日期
-     */
-    List<String> getDateList(String app, String stream, int year, int month, List<MediaServer> mediaServerItems);
+        /**
+         * 获取所有的日期
+         */
+        List<String> getDateList(String app, String stream, int year, int month, List<MediaServer> mediaServerItems);
 
+<<<<<<< HEAD
     /**
      * 添加合并任务
      */
@@ -43,27 +53,63 @@ public interface ICloudRecordService {
      * 收藏视频，收藏的视频过期不会删除
      */
     int changeCollect(boolean result, String app, String stream, String mediaServerId, String startTime, String endTime, String callId);
+=======
+        /**
+         * 添加合并任务
+         */
+        String addTask(String app, String stream, MediaServer mediaServerItem, String startTime,
+                        String endTime, String callId, String remoteHost, boolean filterMediaServer);
 
-    /**
-     * 添加指定录像收藏
-     */
-    int changeCollectById(Integer recordId, boolean result);
+        /**
+         * 查询合并任务列表
+         */
+        JSONArray queryTask(String app, String stream, String callId, String taskId, String mediaServerId,
+                        Boolean isEnd,
+                        String scheme);
 
-    /**
-     * 获取播放地址
-     */
-    DownloadFileInfo getPlayUrlPath(Integer recordId);
+        /**
+         * 收藏视频，收藏的视频过期不会删除
+         */
+        int changeCollect(boolean result, String app, String stream, String mediaServerId, String startTime,
+                        String endTime,
+                        String callId);
+>>>>>>> e1caa29e2 (取消20倍速播放)
 
+        /**
+         * 添加指定录像收藏
+         */
+        int changeCollectById(Integer recordId, boolean result);
+
+        /**
+         * 获取播放地址
+         */
+        DownloadFileInfo getPlayUrlPath(Integer recordId);
+
+<<<<<<< HEAD
     List<CloudRecordItem> getAllList(String query, String app, String stream, String startTime, String endTime, List<MediaServer> mediaServerItems, String callId, List<Integer> ids);
+=======
+        List<CloudRecordItem> getAllList(String query, String app, String stream, String startTime, String endTime,
+                        List<MediaServer> mediaServerItems, String callId, List<Integer> ids);
+>>>>>>> e1caa29e2 (取消20倍速播放)
 
-    /**
-     * 加载录像文件，形成录像流
-     */
-    void loadRecord(String app, String stream, String date, ErrorCallback<StreamInfo> callback);
+        /**
+         * 加载录像文件，形成录像流
+         */
+        void loadRecord(String app, String stream, String date, ErrorCallback<StreamInfo> callback);
 
+<<<<<<< HEAD
     void seekRecord(String mediaServerId,String app, String stream, Double seek, String schema);
+=======
+        /**
+         * 根据文件索引加载指定的录像文件
+         */
+        void loadRecordByFileIndex(String app, String stream, String date, Integer fileIndex,
+                        ErrorCallback<StreamInfo> callback);
 
-    void setRecordSpeed(String mediaServerId, String app, String stream, Integer speed, String schema);
+        void seekRecord(String mediaServerId, String app, String stream, Double seek, String schema);
+>>>>>>> e1caa29e2 (取消20倍速播放)
 
-    void deleteFileByIds(Set<Integer> ids);
+        void setRecordSpeed(String mediaServerId, String app, String stream, Integer speed, String schema);
+
+        void deleteFileByIds(Set<Integer> ids);
 }
